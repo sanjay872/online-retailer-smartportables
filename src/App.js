21193a621f5ed6dfc91225fb9f9437da3af9e33b
Sanjay Sakthivel <sanjaysakthivel2000@gmail.com>
2025-09-13 17:51:35 -0500

    styling completed

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
         <BrowserRouter>
           <Header></Header>
           <Routes>
--- src/view/homePage.jsx
+++ src/view/homePage.jsx
@@ -1,22 +1,12 @@
 import '../styles/homepage.css';
 
 function HomePage(){
-
-    const collageImages=[]
-
-    for(let i=1;i<=4;i++){
-        collageImages.push(<img key={i} className={'home_right_side-image-'+i} src={require('../images/home_bg-'+i+'.jpg')} alt={'Home BG - '+i}></img>);
-    }
-
     return(
         <div className="home">
             <div className="home_left_side">
-                <div className="home_left_side-title">online retailer SmartPortables</div>
+                <div className="home_left_side-title">online retailer <br/> SmartPortables</div>
             </div>
             <div className="home_right_side">
-                <div className='home_right_side-wrapper'>
-                    {collageImages}
-                </div>
                 <div className="home_right_side-description">
                     Everything You Need, <br/> 
                     Everywhere You Go.
