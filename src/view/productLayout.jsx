1f803eec683d4fed7b09fd314a9e50828ed2de32
Sanjay Sakthivel <sanjaysakthivel2000@gmail.com>
2025-10-03 14:02:48 -0500

    working on cart feature

--- src/view/productLayout.jsx
+++ src/view/productLayout.jsx
@@ -1,4 +1,4 @@
-import { Outlet } from "react-router-dom";
+import { Outlet} from "react-router-dom";
 import ProductList from "./productList";
 import { useState } from "react";
 
--- src/view/productViewPage.jsx
+++ src/view/productViewPage.jsx
@@ -7,6 +7,31 @@ export default function ProductViewPage(){
 
     const  image = require("../images/"+product.imageId);
 
+    function addItemToStorage(){
+        const existCart=localStorage.getItem("cart");
+
+        if(existCart!=null){
+            const cartObj=JSON.parse(existCart);
+            let foundProduct=false;
+            cartObj.forEach((item)=>{
+                if(item.id===product.id){
+                    item.quantity++;
+                    foundProduct=true;
+                    return;
+                }
+            });
+            if(!foundProduct)
+                cartObj.push({...product,quantity:1});
+            localStorage.setItem("cart",JSON.stringify(cartObj));
+        }
+        else{
+            var cart=[]
+            cart.push({...product,quantity:1});
+            const stringCart=JSON.stringify(cart);
+            localStorage.setItem("cart",stringCart);
+        }
+    }
+
     return(
         <div className="product">
             <div className="product_leftside">
@@ -24,6 +49,7 @@ export default function ProductViewPage(){
                     <div className="product_rightside_end-price">${product.price}</div>
                     <div className="product_rightside_end-stock">Left out {product.stock}</div>
                 </div>
+                <button className="cart_btn" onClick={addItemToStorage}>Add to Cart</button>
             </div>
         </div>
     );
